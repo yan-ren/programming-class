8974634dc3f81a7fca039fc19613ef3fb9ca7f00
Yan Ren <ryan.ren3@gmail.com>
2020-03-10 21:45:30 -0400

    upload examples

---
+++ python_demo_programs/break_continue.py
@@ -0,0 +1,16 @@
+# a = range(3)
+# print(list(a))
+
+# range(10) -> [0, 1, 2, ..., 9]
+
+# for i in range(10):
+#     if i == 5:
+#         break
+#     print(i, "hello")
+#
+# print('finish')
+
+# for i in range(10):
+#     if i == 6:
+#         continue
+#     print(i)
