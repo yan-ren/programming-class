69cb5f0b1b5d4a5a3c8b3366aa66b82b6c22b796
Yan Ren <ryan.ren3@gmail.com>
2020-04-11 17:56:08 -0400

    add examples

--- python_demo_programs/pygame_examples/pygame_intro.py
+++ python_demo_programs/pygame_examples/pygame_intro.py
@@ -2,6 +2,8 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+FPS = 60
+clock = pygame.time.Clock()
 done = False
 
 # RGB = R - Red, G - Green, B - Blue
@@ -10,16 +12,35 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
+cat_img = pygame.image.load('cat2.png')
+cat_x = 10
+cat_y = 10
+direction = 'right'
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    screen.fill(WHITE)
-    #                              x,   y,  width, height
-    pygame.draw.rect(screen, RED, [40, 200, 100, 70], 1)
-    pygame.draw.rect(screen, GREEN, [200, 200, 100, 100], 0)
-    pygame.draw.line(screen, BLACK, [0, 0], [400, 300], 5)
+    if direction == 'right':
+        cat_x += 5
+        if cat_x == 720:
+            direction = 'down'
+    elif direction == 'down':
+        cat_y += 5
+        if cat_y == 520:
+            direction = 'left'
+    elif direction == 'left':
+        cat_x -= 5
+        if cat_x == 10:
+            direction = 'up'
+    elif direction == 'up':
+        cat_y -= 5
+        if cat_y == 10:
+            direction = 'right'
 
+    screen.fill(WHITE)
+    screen.blit(cat_img, (cat_x, cat_y))
     # update the screen
     pygame.display.update()
+    clock.tick(FPS)
