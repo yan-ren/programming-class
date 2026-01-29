c5338e9e1096aaaf3d1bacefa86a16eb7cfba7fa
Yan Ren <ryan.ren3@gmail.com>
2026-01-29 18:08:48 -0500

    Add notes

---
+++ python_demo_programs/class_2024_09_14_Aaron/2026/class_0122.py
@@ -0,0 +1,19 @@
+'''
+3 10 12 5
+'''
+distance = list(map(int, input().split())) # '3 10 12 5' -> ['3', '10', '12', '5'] -> [3, 10, 12, 5]
+
+distance_a = [0, 0, 0, 0, 0]
+distance_a[1] = distance[0]
+distance_a[2] = distance[0] + distance[1]
+distance_a[3] = distance[0] + distance[1] + distance[2]
+distance_a[4] = distance[0] + distance[1] + distance[2] + distance[3]
+
+print(' '.join(map(str, distance_a)))
+
+for i in range(1, len(distance_a)):
+    result = []
+    for j in range(len(distance_a)):
+        result.append(abs(distance_a[j] - distance_a[i]))
+    print(' '.join(map(str, distance_a)))
+    
